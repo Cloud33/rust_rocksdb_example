597d40f699cd858cb143c2356485a6674bae78ab
fenglin <userfl@live.com>
2022-05-07 19:19:14 +0800

    add

--- README.md
+++ README.md
@@ -10,3 +10,16 @@ https://github.com/facebook/rocksdb/wiki/Building-on-Windows
 https://crates.io/crates/rocksdb
 
 https://github.com/rust-rocksdb/rust-rocksdb/issues/381
+
+get_pinned
+https://github.com/rust-rocksdb/rust-rocksdb/pull/345
+
+
+## id
+yymmddhhmmssfff
+220504170416111
+6887650413378670482 
+
+
+
+https://crates.io/crates/rs-snowflake
--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,13 @@ fn main() {
     db_opts.create_missing_column_families(true);
     db_opts.create_if_missing(true);
     {
-
-        
-
        let db = DB::open_cf_descriptors(&db_opts, path, vec![cf]).unwrap();
-      
+     
        let cf1=  db.cf_handle("cf1").unwrap();
+       
+       let key = 1234_i64.to_ne_bytes();
+       db.put_cf(&cf1,&key, "value").unwrap();
+
        db.put_cf(&cf1,b"my key", b"my value2").unwrap();
        match db.get_pinned_cf(&cf1,b"my key") {
            Ok(Some(value)) => {
@@ -29,7 +30,14 @@ fn main() {
            Ok(None) => println!("value not found"),
            Err(e) => println!("operational problem encountered: {}", e),
        }
+
+       println!("key={} value{}",i64::from_be_bytes(key),String::from_utf8(db.get_pinned_cf(&cf1,&key).unwrap().unwrap().as_ref().to_vec()).unwrap());
+
+
        db.delete_cf(&cf1,b"my key").unwrap();
+       db.delete_cf(&cf1,&key).unwrap();
     }
+
+
     let _ = DB::destroy(&Options::default(), path);
 }
